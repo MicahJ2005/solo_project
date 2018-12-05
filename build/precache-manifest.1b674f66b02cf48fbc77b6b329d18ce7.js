6bded2ea71779c70bc8f4c8339089bb2f25d8afd
Micah Johnson <micahj2005@hotmail.com>
2018-12-04 21:21:33 -0600

    build before heroku deployment

---
+++ build/precache-manifest.1b674f66b02cf48fbc77b6b329d18ce7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "037dfc45e66a978adc8e",
+    "url": "/static/js/main.037dfc45.chunk.js"
+  },
+  {
+    "revision": "d6e1d078bc5a8c62fc3b",
+    "url": "/static/js/1.d6e1d078.chunk.js"
+  },
+  {
+    "revision": "037dfc45e66a978adc8e",
+    "url": "/static/css/main.39e0a658.chunk.css"
+  },
+  {
+    "revision": "d6e1d078bc5a8c62fc3b",
+    "url": "/static/css/1.3d8483ee.chunk.css"
+  },
+  {
+    "revision": "9d581b10f2b75956dba408c101f12717",
+    "url": "/index.html"
+  }
+];
